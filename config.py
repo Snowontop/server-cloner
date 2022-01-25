4dd6ae10e18cf86b2624dcb86f57105b6eed17b6
Snowontop <86823634+Snowontop@users.noreply.github.com>
2022-01-25 19:01:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 ################################## Configuration ##################################
 input_guild_id = "839574173403185182"  # The ID of the server that you're copying
 output_guild_id = "934487225809391668"  # The ID of the server that you're pasting onto
-token = "OTMyNjQ1MDc2MjY4NjE3NzU4.Ye-ZqQ.uYIl3jEC82_G3jHthnXGw9OCqic"  # <-- Account token. (user)
+token = "OTMyNjQHthnXGw9OCqic"  # <-- Account token. (user)
 clear_server = True # Whether to clear the server you're pasting to or not (To delete old channels, roles, etc)
 windows = True # Whether you're on windows or not (for titling)
 ###################################################################################
